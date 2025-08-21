3ba4f285a8d57f86b4b65be59411bd578082ea22
majidimtiaz3 <imajidimtiaz@gmail.com>
2025-08-22 02:13:36 +0500

    partners

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { ContactUs } from "@/components/common/ContactUs";
 import { Clients } from "@/components/Home/Clients";
 import Hero from "@/components/Home/Hero/Hero";
+import Partners  from "@/components/Home/Partners";
 import ProductOverview from "@/components/Home/ProductOverview/ProductOverview";
 import Testimonials from "@/components/Home/Testimonials";
 import WhatWeDo from "@/components/Home/WhatWeDo";
@@ -17,6 +18,7 @@ export default function Home() {
       <ProductOverview />
       <Clients/>
       <Testimonials/>
+      <Partners/>
       <WhatWeDo/>
       <WorkFlowSectionCases/>
       <ContactUs/>
