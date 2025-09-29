3f109d1086659e8d78adf845ef2733f0ce4d82b6
wackar-afzal <waqarafzalrai@gmail.com>
2025-09-29 20:20:42 +0500

    uui

--- src/components/common/SkeletonImage.jsx
+++ src/components/common/SkeletonImage.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useRef, useEffect } from 'react';
 import ImageSkeleton from './ImageSkeleton';
-cases
 // Cache to store loaded images
 const imageCache = new Map();
 
